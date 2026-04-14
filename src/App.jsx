84f21d2bac1d8e2992b2e983f7851640309d5d83
dibsan <un.sanbiters@gmail.com>
2026-04-15 03:26:30 +0530

    spotify track removed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import FloatingCVButton from "./components/FloatingCVButton";
-import SpotifySection from "./components/SpotifySection";
+
 
 export default function App() {
   return (
@@ -24,7 +24,7 @@ export default function App() {
       <GlobalJourney />
       <FeaturedMoments />
       <Gallery />
-      <SpotifySection />
+     
       <Contact />
 
       <Footer />
